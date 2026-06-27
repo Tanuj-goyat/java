02a2623e6232f27e9b42ea4217eac8c9d349b845
Tanuj-Goyat <tanujgoyat640@gmail.com>
2026-06-27 11:49:31 +0530

    Packages

---
+++ OOPS/Module/Demo.java
@@ -0,0 +1,9 @@
+import collage.Student;
+public class Demo {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.print();
+        school.Student s2 = new school.Student();
+        s2.print();
+    }
+}
---
+++ OOPS/Module/collage/Student.java
@@ -0,0 +1,8 @@
+package collage;
+
+public class Student {
+    public void print() {
+        System.out.println("Collage Student");
+    }
+    
+}
